e63d245d898ec385d282466ca1f81b3a6ffa7cdf
Rizzyrizz <105413177+Rizzyrizz@users.noreply.github.com>
2022-05-12 15:28:01 +0900

    Add files via upload

---
+++ server.py
@@ -0,0 +1,16 @@
+import asyncio
+import websockets
+
+async def server(websocket):
+    # Get received data from websocket
+    data = await websocket.recv()
+
+    # Send response back to client to acknowledge receiving message
+    await websocket.send("Thanks for your message: " + data)
+
+# Create websocket server
+start_server = websockets.serve(server, "localhost", 3001)
+
+# Start and run websocket server forever
+asyncio.get_event_loop().run_until_complete(start_server)
+#asyncio.get_event_loop().run_forever()
